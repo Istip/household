f416cbeb911f649b95baabf4ce0d83a5e7a2d8bd
Istip <isticsek@gmail.com>
2022-07-15 09:19:27 +0300

    Added timeformatter

--- frontend/src/components/items/Item.tsx
+++ frontend/src/components/items/Item.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
+import { Avatar, Box, Button, Divider, Text } from '@chakra-ui/react';
+import { useAuth } from '../../context/AuthContext';
 import { Item as IItem } from '../../interfaces/Item';
+import dayjs from 'dayjs';
 
 interface IProps {
   item: IItem;
 }
 const Item: React.FC<IProps> = ({ item }) => {
+  const { user } = useAuth();
+
   return (
     <Box
       p={5}
@@ -13,7 +17,7 @@ const Item: React.FC<IProps> = ({ item }) => {
       borderRadius={10}
       mb={2}
     >
-      <Flex justifyContent="space-between" alignItems="center">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
         <Box fontWeight="bold" ml={1}>
           {item.name}
         </Box>
@@ -25,7 +29,33 @@ const Item: React.FC<IProps> = ({ item }) => {
             <i className="fa-solid fa-circle-check"></i>
           </Button>
         </Box>
-      </Flex>
+      </Box>
+      <Box my={3}>
+        <Divider />
+      </Box>
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Avatar name={user!.name} size="2xs" />
+
+        <Box>
+          <Text
+            fontSize="xs"
+            fontWeight="bold"
+            color={item.completed ? 'green.400' : 'red.400'}
+            mr={1}
+          >
+            {item.completed ? 'COMPLETED' : 'INCOMPLETE'}
+          </Text>
+        </Box>
+
+        <Text fontSize="xs">
+          <Text fontSize="xs" as="span" color="gray.400" mr={1}>
+            <i className="fa-solid fa-clock"></i>
+          </Text>
+          <Text fontSize="xs" as="span" color="gray.400" mr={1}>
+            {dayjs(item.createdAt).format('MMMM DD : HH:mm')}
+          </Text>
+        </Text>
+      </Box>
     </Box>
   );
 };
