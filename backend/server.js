326d307b7e095599824b88b77f792cb82f860bb2
Istip <isticsek@gmail.com>
2022-11-14 10:58:12 +0200

    Test comment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const { errorHandler } = require('./middlewares/errorMiddleware');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
+// test comment
 // connect to database
 connectDB();
 
